5172d5ab4aea39f3c3a9b928233f4b10647bab4a
s19541 <s19541@LAB003-08>
2020-03-09 11:30:16 +0100

    Z zajec

--- Cwiczenia2/Cwiczenia2/Program.cs
+++ Cwiczenia2/Cwiczenia2/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace Cwiczenia2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String file = @"Data\dane.csv";
+
+            FileInfo f = new FileInfo(file);
+            try
+            {
+                StreamReader stream = new StreamReader(f.OpenRead());
+                string line = "";
+                while ((line = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            //XML
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+            var list = new List<Student>();
+            var s=new Student
+            {
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "kowalski@wp.pl"
+            };
+            list.Add(s);
+            serializer.Serialize(writer, list);
+            Console.WriteLine("koniec");
+
         }
     }
 }
---
+++ Cwiczenia2/Cwiczenia2/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Cwiczenia2
+{
+   public class Student
+    {
+        [XmlElement(elementName:"fname")]
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        [XmlAttribute(attributeName:"email")]
+        public string Email { get; set; }
+    }
+}
